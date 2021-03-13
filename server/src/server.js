71028e9548e6c6357ec4f1cc6c6973aafb1e1b88
Wesley Paulo <angelportinari17@gmail.com>
2021-03-13 17:01:56 -0300

    Trying Heroku Deployment with Github Actions

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,6 @@
 const server = require('./app')
-const { SERVER_PORT: PORT, SERVER_HOST: HOST } = process.env
-server.listen(PORT || 3000, HOST || '0.0.0.0', () => console.log(`Running server on ${PORT}`))
+const { SERVER_PORT, SERVER_HOST } = process.env
+server.listen(process.env.PORT || SERVER_PORT,
+  SERVER_HOST || '0.0.0.0',
+  () => console.log(`Running server on ${SERVER_PORT}`)
+)
